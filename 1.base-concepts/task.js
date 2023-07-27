9d10c4a9442f79b71c651e7763e5ca4f11465b21
Ne1po2 <subway91@rambler.ru>
2023-07-27 10:26:44 +0900

    test3

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,18 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
+  let dis = b ** 2 - 4 * a * c;
+  if (dis < 0) {
+    return arr;
+  } else if (dis == 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(dis) )/(2*a));
+    arr.push((-b - Math.sqrt(dis) )/(2*a));
+  }
   return arr;
-  
 }
-console.log(123);
+
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
 }
