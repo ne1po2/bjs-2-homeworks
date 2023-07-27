85c2e988a2a6eb7d40dbc643b47dabe9b22631e5
Ne1po2 <subway91@rambler.ru>
2023-07-27 11:44:35 +0900

    test5

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -18,28 +18,10 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
     let p = percent / 100 / 12;
     let s = amount - contribution;
     let total = (s * (p + (p / (((1 + p) ** countMonths) - 1)))) * countMonths;
-    return total.toFixed(2);
+    total = parseFloat(total.toFixed(2));
+    if (isNaN(total)) {
+        return false;
+    } else {
+        return total;
+    }
 }
-
-// console.log(calculateTotalMortgage(10, 0, 50000, 12));
-// console.log(calculateTotalMortgage(10, 1000, 50000, 12));
-// console.log(calculateTotalMortgage(10, 20000, 20000, 48));
-// console.log(calculateTotalMortgage(10, 0, 10000, 36));
-// console.log(calculateTotalMortgage(15, 0, 10000, 36));
-// expect(calculateTotalMortgage(10, 0, 50000, 12)).toEqual(52749.53);
-//     });
-
-//     it('верно считать кредит: кейс #2', () => {
-//       expect(calculateTotalMortgage(10, 1000, 50000, 12)).toEqual(51694.54);
-//     });
-
-//     it('верно считать кредит: кейс #3', () => {
-//       expect(calculateTotalMortgage(10, 20000, 20000, 48)).toEqual(0);
-//     });
-
-//     it('верно считать кредит: кейс #4', () => {
-//       expect(calculateTotalMortgage(10, 0, 10000, 36)).toEqual(11616.19);
-//     });
-
-//     it('верно считать кредит: кейс #5', () => {
-//       expect(calculateTotalMortgage(15, 0, 10000, 36)).toEqual(12479.52);
