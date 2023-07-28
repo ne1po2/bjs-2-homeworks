357f004d80f23665b61a33b501f54cf0337303dc
Ne1po2 <subway91@rambler.ru>
2023-07-28 11:21:28 +0900

    test2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,13 +1,13 @@
 function compareArrays(arr1, arr2) {
-	if (arr1.length != arr2.length) {
-		return false;
-	}
-	return arr1.every((value, index) => value == arr2[index]);
+  if (arr1.length != arr2.length) {
+    return false;
+  }
+  return arr1.every((value, index) => value == arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-	let filteredUsers = users.filter((user) => user.gender == gender);
-	if (filteredUsers.length === 0) {
+  let filteredUsers = users.filter((user) => user.gender == gender);
+  if (filteredUsers.length === 0) {
     return 0;
   }
   let totalAge = filteredUsers.reduce((sum, user) => sum + user.age, 0);
