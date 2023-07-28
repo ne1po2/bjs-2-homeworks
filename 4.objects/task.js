1869e1776d4c0ea71739d4ad3558e9a507ed1705
Ne1po2 <subway91@rambler.ru>
2023-07-28 14:53:35 +0900

    test

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function(...marks) {
+  if (!this.marks) {
+    return;
+  }
+  this.marks.push(...marks);
 }
 
-Student.prototype.getAverage = function () {
-  
+Student.prototype.getAverage = function() {
+  if (!this.marks || this.marks.length == 0) {
+    return 0;
+  }
+  let totalMarks = this.marks.reduce((sum, mark) => sum + mark, 0);
+  let result = totalMarks / this.marks.length;
+  return result;
 }
 
-Student.prototype.exclude = function (reason) {
-  
+Student.prototype.exclude = function(reason) {
+  delete this.marks;
+  delete this.subject
+  this.excluded = reason;
 }
