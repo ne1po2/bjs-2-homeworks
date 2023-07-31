e4716e0d7b4652fd144be6e7e80f1ac925c32097
Ne1po2 <subway91@rambler.ru>
2023-07-31 15:33:01 +0900

    test

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,60 @@
-//Задача № 1
 function cachingDecoratorNew(func) {
-  
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = md5(args); 
+    let objectInCache = cache.find((item) => item.hash === hash); 
+    if (objectInCache) { 
+      console.log("Из кеша: " + objectInCache.result);
+      return "Из кеша: " + objectInCache.result;
+    }
+
+    let result = func(...args); 
+    cache.push({ hash, result }); 
+    if (cache.length > 5) {
+      cache.shift(); 
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper;
 }
 
-//Задача № 2
-function debounceDecoratorNew(func, delay) {
-  
+function debounceDecoratorNew(func, timeout) {
+  let timerId;
+  let count = 0;
+  let allCount = 0;
+
+  function wrapper(...args) {
+    clearTimeout(timerId);
+    const isFirstCall = !timerId;
+
+    if (isFirstCall) {
+      func(...args);
+      count++;
+    }
+
+    timerId = setTimeout(() => {
+      timerId = null;
+      if (!isFirstCall) {
+        func(...args);
+      }
+    }, timeout);
+
+    allCount++;
+  }
+
+  Object.defineProperty(wrapper, 'count', {
+    get: function () {
+      return count;
+    },
+  });
+
+  Object.defineProperty(wrapper, 'allCount', {
+    get: function () {
+      return allCount;
+    },
+  });
+
+  return wrapper;
 }
